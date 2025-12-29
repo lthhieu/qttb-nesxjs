d91454e24c0da0b966f4a499a55735e5ec0b85dc
Ly Tran Hoang Hieu <lyhieu2024@gmail.com>
2025-12-29 14:17:21 +0700

    dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,9 @@
-import CopyPlugin from "copy-webpack-plugin";
 import type { NextConfig } from "next";
-import path from "node:path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  turbopack: {},
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals = config.externals || [];
-    }
-    if (isServer) {
-      config.externals.push('canvas');
-    } return config
-  }
+  output: "standalone",
 };
 
 export default nextConfig;
